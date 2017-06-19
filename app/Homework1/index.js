39fe153c7b82ca084a6641b12e29b4c415ec2189
Aaron Harrington <aharrington@trustarc.com>
2017-06-19 15:26:10 -0700

    Did homework 2

--- app/Homework1/index.js
+++ app/Homework1/index.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types'
 import Radium from 'radium'
 import color from 'color'
 import postData from './postData.js'
+import { Provider, connect } from 'react-redux'
+import { createStore } from 'redux'
+import _ from 'lodash'
 
 /*
    Homework 1: Rendering Data
@@ -34,22 +37,81 @@ const styles = {
   title: {
     fontSize: "2em",
     textAlign: "center"
+  },
+
+  footer: {
+    fontSize: ".8em",
+    textAlign: "right"
   }
 
 }
 
-const PostList = ({data}) => (
+const reducer = (state, action) => {
+  if(_.isNil(state)) {
+    return {
+      postList: postData,
+      title: "",
+      body: ""
+    }
+  }
+
+  console.log("hi", action)
+
+  switch(action.type) {
+    case "ADD": {
+      let postListCopy = _.cloneDeep(state.postList);
+      postListCopy.push(action.value);
+      return { ...state, postList: postListCopy};
+    }
+    case "REMOVE": {
+      let postListCopy = _.cloneDeep(state.postList);
+      postListCopy.splice(action.index, 1)
+      return {...state, postList: postListCopy};
+    }
+    case "TITLE_CHANGE": {
+      return { ...state, title: action.value}
+    }
+    case "BODY_CHANGE": {
+      return { ...state, body: action.value} 
+    }
+  }
+}
+
+const addPost = (postValue) => ({
+  type: "ADD",
+  value: postValue
+});
+
+const removePost = (postIndex) => ({
+  type: "REMOVE",
+  index: postIndex
+});
+
+const titleChange = (title) => ({
+  type: "TITLE_CHANGE",
+  value: title.target.value
+});
+
+const bodyChange = (body) => ({
+  type: "BODY_CHANGE",
+  value: body.target.value
+})
+
+const store = createStore(reducer);
+
+const PostList = ({data, deletePost}) => (
   <ul>
     {data.map((val, i) => (
       <li key={val.id} style={styles.post}>
-        <h1 style={styles.title}>{val.userId}: {val.title}</h1>
+        <h1 style={styles.title}>{val.title}</h1>
         <p>{val.body}</p>
+        <div style={styles.footer}>Created by: {val.userId} <button onClick={() => deletePost(i)}>delete</button></div>
       </li>
     ))}
   </ul>
 )
 
-const SortedPostList = ({sortMethod, data}) => {
+const SortedPostList = ({sortMethod, data, deletePost}) => {
   let sortedData = data.sort((a, b) => {
     if(typeof a[sortMethod] === "string"){
       return a[sortMethod].localeCompare(b[sortMethod]);
@@ -58,14 +120,46 @@ const SortedPostList = ({sortMethod, data}) => {
     }
   })
 
-  return <PostList data={sortedData} />
+  return <PostList data={sortedData} deletePost={deletePost}/>
+}
+
+let PostPage = ({sortMethod, postList, titleText, bodyText, addPost, deletePost, bodyChange, titleChange}) => (
+  <div>
+    <h2>Title</h2>
+    <input onChange={titleChange}></input>
+    <h2>Body</h2>
+    <input onChange={bodyChange}></input>
+    <button onClick={() => addPost({userId: 1, title: titleText, body: bodyText, id: postList.length + 1})}>Add Post</button>
+    <div>
+      {sortMethod ? <SortedPostList sortMethod={sortMethod} data={postList} deletePost={deletePost}/> : <PostList data={postList} deletePost={deletePost}/>}
+    </div>
+  </div>
+)
+
+const mapStateToProps = (state) => {
+  return {
+    titleText: state.title,
+    bodyText: state.body,
+    postList: state.postList
+  }
 }
 
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addPost: (post) => dispatch(addPost(post)),
+    deletePost: (index) => dispatch(removePost(index)),
+    bodyChange: (body) => dispatch(bodyChange(body)),
+    titleChange: (title) => dispatch(titleChange(title))
+  }
+}
+
+PostPage = connect(mapStateToProps, mapDispatchToProps)(PostPage)
+
 const Homework1 = ({sortMethod}) => (
-  <div>
-    {sortMethod ? <SortedPostList sortMethod={sortMethod} data={postData} /> : <PostList data={postData} />}
-  </div>
+  <Provider store={store}>
+    <PostPage sortMethod={sortMethod} />
+  </Provider>
 )
 
 Homework1.propTypes = {
